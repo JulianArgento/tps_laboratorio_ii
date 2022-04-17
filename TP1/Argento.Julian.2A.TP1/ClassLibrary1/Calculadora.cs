480dbce4523df48b9abb262532f9bf3cdf85d44d
JulianArgento <julianargento123@gmail.com>
2022-04-17 11:39:59 -0300

    TP1 completo

---
+++ TP1/Argento.Julian.2A.TP1/ClassLibrary1/Calculadora.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        /// <summary>
+        /// Valida el operador pasado por parametro
+        /// </summary>
+        /// <param name="operador">Operador a validar</param>
+        /// <returns>retorna el operador ingresado en caso de ser valido, retorna el operador '+' en caso de que el ingresado
+        /// sea invalido</returns>
+        private static char ValidarOperador(char operador)
+        {
+            if (operador != '+' && operador != '-' && operador != '/' && operador != '*')
+            {
+                return '+';
+            }
+
+
+                return operador;
+        }
+
+
+        /// <summary>
+        /// Realiza la operacion entre los numeros ingresados
+        /// </summary>
+        /// <param name="num1">Primer numero a operar</param>
+        /// <param name="num2">Segundo numero a operar</param>
+        /// <param name="operador">Operador del calculo que se desee realizar</param>
+        /// <returns>retorna el resultado de la cuenta, en caso de que el operador no sea valido retornara un 0</returns>
+        public static double Operar(Operando num1, Operando num2 , char operador)
+        {
+            
+            char op = ValidarOperador(operador);
+
+
+
+
+
+            switch(op)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '/':
+                    return num1 / num2;
+                case '*':
+                    return num1 * num2;
+
+            }
+
+
+           
+
+            return 0;
+
+        }
+
+
+    }
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+}
---
+++ TP1/Argento.Julian.2A.TP1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+
+        }
+
+       
+
+        /// <summary>
+        /// Limpia el formulario al cargarlo 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormCalculadora_Load(object sender, System.EventArgs e)
+        {
+            Limpiar();
+        }
+
+        /// <summary>
+        /// valida que los 2 numeros ingresados sean double, asigna los numeros a 2 nuevos objetos operando y realiza la operacion entre
+        /// esos objetos, utilizando el operador ingresado
+        /// </summary>
+        /// <param name="numero1">primer operando</param>
+        /// <param name="numero2">segundo operando</param>
+        /// <param name="operador">operador de la cuenta</param>
+        /// <returns>retorna el resultado de la cuenta en caso de ser numeros, retorna NaN en caso de que se hayan recibido letras</returns>
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            //Outs del parse
+            double num1;
+            double num2;
+
+            if (Double.TryParse(numero1, out num1)&& Double.TryParse(numero2, out num2))
+            {
+                Operando operando1 = new Operando(numero1);
+
+                Operando operando2 = new Operando(numero2);
+
+                return Calculadora.Operar(operando1, operando2, operador[0]);
+            }
+
+            
+
+
+            return double.NaN;
+         
+        }
+
+
+        /// <summary>
+        /// Limpia los campos de texto del form, a excepcion del historial
+        /// </summary>
+        private void Limpiar()
+        {
+            txtNumero1.Text = "";
+
+            txtNumero2.Text = "";
+
+            cmbOperador.SelectedItem = "";
+
+            lblResultado.Text = "";
+        }
+
+        
+        /// <summary>
+        /// Al clickear en el boton de limpiar, llama al metodo limpiar para liberar los campos de texto
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+            Limpiar();
+
+
+        }
+
+        /// <summary>
+        /// Al clickear en operar, lee los operandos y el operador, llamando al m etodo operar y retornando el valor de la operacion
+        /// en el label resultado. Tambien, agrega la operacion a la lista de operaciones
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+                double resultado = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+
+            
+                string mensaje = resultado.ToString();
+
+                //Mostrar el resultado
+                lblResultado.Text = mensaje;
+
+                //Añadir la operacion a la lista de operaciones
+                lstOperaciones.Items.Add(txtNumero1.Text + " " + cmbOperador.Text + " " + txtNumero2.Text + " = " + mensaje);
+            
+                 
+
+        }
+
+        /// <summary>
+        /// Cierra el form al clickear el boton Cerrar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Llama al metodo DecimalBinario cuando se clickea en el boton de convertir a binario
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+
+            Operando op = new Operando();
+
+
+            if (lblResultado.Text != "Valor invalido")
+            {
+                lblResultado.Text = op.DecimalBinario(Double.Parse(lblResultado.Text));
+            }
+        }
+
+
+
+        /// <summary>
+        /// Llama al metodo BinarioDecimal cuando se clickea en el boton de convertir a decimal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Operando op = new Operando();
+
+            if (lblResultado.Text != "Valor invalido")
+            {
+                lblResultado.Text = op.BinarioDecimal(lblResultado.Text);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Muestra al usuario una ventana de confirmacion de salida.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            DialogResult dr = MessageBox.Show("Seguro que quiere cerrar el formulario?", "EXIT", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (dr == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+
+
+        }
+    }
+}
